ddc4df7a7dac89e5f5cd60a372c6aa47d04655a9
krislintigo <krislintigo@gmail.com>
2026-04-26 13:12:21 +0300

    chore: update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,92 +1,13 @@
-import pluginJs from '@eslint/js'
-import tseslint from 'typescript-eslint'
-import sonarjs from 'eslint-plugin-sonarjs'
-import pluginImport from 'eslint-plugin-import'
-import eslintConfigPrettier from 'eslint-config-prettier'
-import globals from 'globals'
+import { createConfig } from '@krislintigo/eslint-config'
 
-export default tseslint.config(
-  {
-    ignores: ['**/dist/**', '**/node_modules/**'],
-  },
-
-  {
-    files: ['**/*.ts'],
-    extends: [
-      pluginJs.configs.recommended,
-      ...tseslint.configs.strictTypeChecked,
-      ...tseslint.configs.stylisticTypeChecked,
-      pluginImport.flatConfigs.recommended,
-      pluginImport.flatConfigs.typescript,
-      sonarjs.configs.recommended,
-    ],
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      globals: globals.browser,
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-        parser: tseslint.parser,
+export default createConfig({
+  extraConfigs: [
+    // Disable redundant type aliases for types files only
+    {
+      files: ['src/**/*.types.ts'],
+      rules: {
+        'sonarjs/redundant-type-aliases': 'off',
       },
     },
-    settings: {
-      'import/resolver': {
-        typescript: {
-          alwaysTryTypes: true,
-          project: './tsconfig.json',
-        },
-        node: true,
-      },
-    },
-    rules: {
-      // IMPORT
-
-      'import/no-unresolved': 'error',
-      'import/no-duplicates': 'error',
-      'import/newline-after-import': 'error',
-      'import/order': [
-        'warn',
-        {
-          groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
-          'newlines-between': 'always',
-          alphabetize: { order: 'asc', caseInsensitive: true },
-        },
-      ],
-
-      '@typescript-eslint/restrict-template-expressions': [
-        'error',
-        {
-          allowNumber: true,
-          allowBoolean: true,
-          allowAny: false,
-          allowNullish: true,
-        },
-      ],
-      '@typescript-eslint/consistent-type-imports': [
-        'error',
-        { prefer: 'type-imports', disallowTypeAnnotations: false },
-      ],
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          caughtErrorsIgnorePattern: '^_',
-        },
-      ],
-
-      'sonarjs/no-implicit-dependencies': 'error',
-      'sonarjs/todo-tag': 'warn',
-    },
-  },
-
-  {
-    files: ['src/**/*.types.ts'],
-    rules: {
-      'sonarjs/redundant-type-aliases': 'off',
-    },
-  },
-
-  eslintConfigPrettier,
-)
+  ],
+})

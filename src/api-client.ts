40a480e8fc91385583a26562703901c0a5b2b0e9
krislintigo <krislintigo@gmail.com>
2026-05-02 09:58:59 +0300

    tests: remove unused code

--- src/api-client.ts
+++ src/api-client.ts
@@ -51,7 +51,3 @@ export class ZenMoneyApiClient {
     )
   }
 }
-
-const client = new ZenMoneyApiClient()
-
-const res = await client.suggest({ accessToken: 'secret-token', payload: { payee: 'Gippo' } })
